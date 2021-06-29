f7b3151d9770b1eb4175b9724cdf982010b9c5d7
Baleine <lj2000lj@126.com>
2021-06-29 16:43:30 +0800

    chore: make plugin.yml auto generated

--- src/main/java/cn/apisium/uniporter/Uniporter.java
+++ src/main/java/cn/apisium/uniporter/Uniporter.java
@@ -5,10 +5,19 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import org.bukkit.plugin.PluginLoadOrder;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.*;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
 import java.util.List;
 
+@Plugin(name = "Uniporter", version = "1.0")
+@Description("A netty wrapper for Minecraft, which allows running multiple protocols in same port.")
+@Author("Baleine_2000")
+@LoadOrder(PluginLoadOrder.STARTUP)
+@Website("https://apisium.cn")
+@ApiVersion(ApiVersion.Target.v1_13)
 public final class Uniporter extends JavaPlugin {
 
     @Override
