0bed028eeb8d173380502c956979612380b945ba
Baleine <lj2000lj@126.com>
2021-07-17 00:47:45 +0800

    fix: non-registered route causes 500

--- src/main/java/cn/apisium/uniporter/router/handler/PreRouteFinder.java
+++ src/main/java/cn/apisium/uniporter/router/handler/PreRouteFinder.java
@@ -5,22 +5,32 @@ import cn.apisium.uniporter.router.api.Route;
 import cn.apisium.uniporter.router.api.UniporterHttpHandler;
 import cn.apisium.uniporter.router.exception.IllegalHttpStateException;
 import cn.apisium.uniporter.router.util.RouteResolver;
-import io.netty.buffer.ByteBufHolder;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.DefaultHttpRequest;
 import io.netty.handler.codec.http.HttpRequest;
 
 public class PreRouteFinder extends SimpleChannelInboundHandler<HttpRequest> implements RouteResolver {
     @Override
     protected void channelRead0(ChannelHandlerContext context, HttpRequest request) throws Exception {
         try {
-            Route route = this.getRoute(context, request.headers(), findPath(request.uri()));
-            UniporterHttpHandler handler = Uniporter.getRouteConfig().getHandler(route.getHandler()).orElse(null);
+            Route route;
+            UniporterHttpHandler handler = null;
+
+            try {
+                route = this.getRoute(context, request.headers(), findPath(request.uri()));
+                if (route == null) {
+                    context.fireChannelRead(request);
+                    return;
+                }
+                handler = Uniporter.getRouteConfig().getHandler(route.getHandler()).orElse(null);
+            } catch (Throwable ignore) {
+            }
+
             if (handler == null || !handler.hijackAggregator()) {
                 context.fireChannelRead(request);
                 return;
             }
+
             handler.hijack(context, request);
             context.channel().pipeline().fireChannelRead(request);
         } catch (Throwable e) {
